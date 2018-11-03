4f01785c8a8f4daaa63a36c7387d6e695070dd53
Raghu K <raghuk.vit@gmail.com>
2018-11-03 01:13:58 +0000

    Updating SHELL-SCRIPTS

---
+++ stack.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+## Script to install webserver and application and server.
+LOG=/tmp/stack.log 
+rm -f $LOG 
+
+## COlor Variables
+R="\e[1;31m"
+G="\e[1;32m"
+Y="\e[1;33m"
+N="\e[0m"
+
+Error() {
+    echo -e "${R}${1}${N}"
+    exit $2
+}
+
+Info() {
+    echo -e -n "$1 "
+}
+
+Stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\t -- ${G}SUCCESS${N}"
+    else 
+        echo -e "\t -- ${R}FAILURE${N}"
+        exit 1
+    fi
+}
+
+LogS() {
+    case $1 in 
+        Head) 
+            echo -e "\n----------------------------------------------------------------------"
+            echo -e "                             $2                                       "
+            echo -e "----------------------------------------------------------------------"
+            ;;
+        Tail) 
+            echo -e "----------------------------------------------------------------------\n"
+}
+
+Run() {
+    Info "$1"
+    LogS Head "$2"
+    $2 &>>$LOG
+    LogS Tail
+    Stat $?
+}
+
+## Check whether the script executed as root user or normal user.
+
+ID=$(id -u)
+if [ "$ID" -ne 0 ]; then 
+    Error "You should be a root user to run this script!!" 2
+fi
+
+Run "Installing HTTPD Server" "yum install httpd -y"  
