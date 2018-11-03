7e5ff6a7705bf5316d7e8814b0bc905ad766b610
Raghu K <raghuk.vit@gmail.com>
2018-11-03 01:32:21 +0000

    Updating SHELL-SCRIPTS

--- stack.sh
+++ stack.sh
@@ -59,5 +59,5 @@ fi
 Run "Installing HTTPD Server" "yum install httpd -y"  
 Run "Setting up Reverse proxy" "curl -f -s https://raw.githubusercontent.com/citb32/project-setup/master/web-proxy.conf -o  /etc/httpd/conf.d/studentapp.conf"
 Run "Setting Up Index File" "curl -f -s https://raw.githubusercontent.com/citb32/project-setup/master/httpd-index.html -o /var/www/html/index.html"
-systemctl enable httpd 
+systemctl enable httpd &>/dev/null
 Run "Start Web Service" "systemctl start httpd"
