298065071eee6e423f389b56afff60205ce4a38c
Raghu K <raghuk.vit@gmail.com>
2018-11-03 01:00:27 +0000

    Updating SHELL-SCRIPTS

--- stack.sh
+++ stack.sh
@@ -20,7 +20,7 @@ if [ "$ID" -ne 0 ]; then
     Error "You should be a root user to run this script!!" 2
 fi
 
- "Installing Web Server"
+Info "Installing Web Server"
 yum install httpd -y &>>$LOG
 if [ $? -eq 0 ]; then
     echo "SUCCESS"
