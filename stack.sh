d06d8176420974ab6f672db3961db5f015c0a1de
Raghu K <raghuk.vit@gmail.com>
2018-11-03 00:57:17 +0000

    Updating SHELL-SCRIPTS

---
+++ stack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## Script to install webserver and application and server.
+LOG=/tmp/stack.log 
+rm -f $LOG 
+
+
+
+Error() {
+    echo -e ""
+}
+
+## Check whether the script executed as root user or normal user.
+
+ID=$(id -u)
+if [ "$ID" -ne 0 ]; then 
+    Error "You should be a root user to run this script!!" 2
+fi
+
+echo -e "Installing Web Server"
+yum install httpd -y &>>$LOG
+if [ $? -eq 0 ]; then
+    echo "SUCCESS"
+else
+    echo "FAILURE"
+fi
