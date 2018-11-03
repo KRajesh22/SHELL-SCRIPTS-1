0f1763c6dc559d316737f43dc8cc55a664e298dc
Raghu K <raghuk.vit@gmail.com>
2018-11-03 00:55:47 +0000

    Updating SHELL-SCRIPTS

---
+++ stack.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Script to install webserver and application and server.
+LOG=/tmp/stack.log 
+rm -f $LOG 
+
+## Check whether the script executed as root user or normal user.
+
+ID=$(id -u)
+if [ "$ID" -ne 0 ]
+
+echo -e "Installing Web Server"
+yum install httpd -y &>>$LOG
+if [ $? -eq 0 ]; then
+    echo "SUCCESS"
+else
+    echo "FAILURE"
+fi
