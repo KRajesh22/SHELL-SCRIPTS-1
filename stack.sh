2a1bf8becce3b9feeb06bdc1d79cf997fee9aced
Raghu K <raghuk.vit@gmail.com>
2018-11-05 01:16:22 +0000

    Updating SHELL-SCRIPTS

--- stack.sh
+++ stack.sh
@@ -105,7 +105,7 @@ su - $TOMCAT_USER -c "wget $APP_URL -O $TOMCAT_DIR/webapps/student.war" &>>$LOG
 Stat $?
 LogS Tail
 
-Info "Downloading JDBC Application"
+Info "Downloading JDBC Jar"
 LogS Head "$2" 
 su - $TOMCAT_USER -c "rm -rf  $TOMCAT_DIR/webapps/*" &>>$LOG
 su - $TOMCAT_USER -c "wget $APP_URL -O $TOMCAT_DIR/webapps/student.war" &>>$LOG
