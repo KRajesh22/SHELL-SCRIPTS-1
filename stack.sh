723deece69f086ccc0f628157c8a70f58b832c3e
Raghu K <raghuk.vit@gmail.com>
2018-11-05 00:56:39 +0000

    Updating SHELL-SCRIPTS

--- stack.sh
+++ stack.sh
@@ -78,6 +78,7 @@ Head "APPLICATION SETUP"
 Run "Installing Java" "yum install java -y"  
 if $(id $TOMCAT_USER) ; then 
     Run "Creating Application User" "useradd $TOMCAT_USER"
+    
 fi
 Info "Downloading Tomcat"
 LogS Head "$2" 
