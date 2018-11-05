bd7df0b040a867a51f6b88b7fad52d72558c9135
Raghu K <raghuk.vit@gmail.com>
2018-11-05 01:12:27 +0000

    Updating SHELL-SCRIPTS

--- stack.sh
+++ stack.sh
@@ -107,7 +107,7 @@ LogS Tail
 
 Info "Configurring Tomcat"
 LogS Head "$2" 
-su - $TOMCAT_USER -c "rm -rf  $TOMCAT_DIR/webapps/*" &>>$LOG
+ "rm -rf  $TOMCAT_DIR/webapps/*" &>>$LOG
 su - $TOMCAT_USER -c "wget $APP_URL -O $TOMCAT_DIR/webapps/student.war" &>>$LOG
 Stat $?
 LogS Tail
