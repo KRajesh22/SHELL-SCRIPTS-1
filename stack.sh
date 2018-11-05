d2c3daaf8641028d7d6194ef6dd8ee58d9be794f
Raghu K <raghuk.vit@gmail.com>
2018-11-05 00:58:27 +0000

    Updating SHELL-SCRIPTS

--- stack.sh
+++ stack.sh
@@ -27,7 +27,7 @@ Info() {
 
 Stat() {
     if [ "$1"  = SKIP ]; then 
-        echo e- "-- ${}"
+        echo e- "-- ${M}"
     elif [ $1 -eq 0 ]; then 
         echo -e "-- ${G}SUCCESS${N}"
     else 
