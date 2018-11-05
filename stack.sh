0201dcc8b1a976862bd3e26953e81c1c70c38211
Raghu K <raghuk.vit@gmail.com>
2018-11-05 01:01:41 +0000

    Updating SHELL-SCRIPTS

--- stack.sh
+++ stack.sh
@@ -96,7 +96,7 @@ LogS Head "$2"
 su - $TOMCAT_USER -c "wget -O- $TOMCAT_URL | tar -xz" &>>$LOG
 Stat $?
 LogS Tail
-Info "Downloading Tomcat"
+Info "Downloading Student Application"
 LogS Head "$2" 
 su - $TOMCAT_USER -c "wget -O- $TOMCAT_URL | tar -xz" &>>$LOG
 Stat $?
